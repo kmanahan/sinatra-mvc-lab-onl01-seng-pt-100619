cd5d3f0e945a6bccdf2777aa5bbc3d8f3629b53e
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-26 13:54:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
   post '/' do 
     erb :results
-     @analyzed_text = TextAnalyzer.new(params[:pig_text])
+     @analyzed_text = Piglatinizer.new(params[:pig_text])
   end 
   
 end
